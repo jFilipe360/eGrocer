1552edca1dede6a408db11b3f66d6a7c66e6cbc1
Joao Correia <aluno21302@ipt.pt>
2024-09-06 14:50:14 +0100

    Adicionar arquivos de projeto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace eGrocer.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
